4a86a6c09af51db51e0ed038026afcebabc42fd5
kelliekumasaka <kelliek3@gmail.com>
2021-10-03 13:51:12 -0700

    displaying icons for current and future weather

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,14 @@ var today = moment().format('L');
 var searchBtn = document.querySelector("#searchBtn");
 var city = document.querySelector("#city");
 var userCity = document.querySelector("#userCity");
+var userIcon = document.querySelector("#icon")
 var userTemp = document.querySelector("#temp")
 var userWind = document.querySelector("#wind")
 var userHumidity = document.querySelector("#humidity")
 var userUv = document.querySelector("#uv")
 var cityName;
 var h5El = document.getElementsByTagName("h5")
+var iconEl = document.querySelectorAll(".cardIcon")
 var humidityEl = document.querySelectorAll(".cardHumidity")
 var tempEl = document.querySelectorAll(".cardTemp")
 var windEl = document.querySelectorAll(".cardWind")
@@ -18,6 +20,7 @@ userCity.textContent = userCity.innerHTML + " " + today;
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
     cityName=city.value.trim();
+    
     getCoordinates();
 })
 
@@ -35,6 +38,8 @@ function getCoordinates(){
             var lat = data.coord.lat
             var lon = data.coord.lon
             var officialCity = data.name
+            var nowIcon = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+            userIcon.setAttribute("src", nowIcon);
             var cityTitle = officialCity + " " + today;
             var secondRequest = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&APPID=cae8444643fe0b52a066739e6b318cbd'
 
@@ -52,13 +57,12 @@ function getCoordinates(){
                         var cardDates = data.daily[i+1].dt;
                         var differentDates = moment(cardDates, "X").format("L");
                         h5El[i].textContent = differentDates;
-                        // daily[i] (for indexes 1-5).humidity
+                        var iconCard = 'http://openweathermap.org/img/wn/' + data.daily[i+1].weather[0].icon + '@2x.png';
+                        iconEl[i].setAttribute("src", iconCard);
                         var humidCard = data.daily[i+1].humidity;
                         humidityEl[i].textContent = "Humidity: " + humidCard + "%";
-                        // daily.temp.max
                         var tempCard = data.daily[i+1].temp.max
                         tempEl[i].textContent = "Temp: " + tempCard + "ºF"
-                        // daily.wind_speed
                         var windCard = data.daily[i+1].wind_speed;
                         windEl[i].textContent = "Wind: " + windCard + " MPH"
                     }
