d8b4ae2e6af8abe814de0dc632d7811af6873056
kelliekumasaka <kelliek3@gmail.com>
2021-10-03 12:59:27 -0700

    five day forecast and card css

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,10 @@ var userWind = document.querySelector("#wind")
 var userHumidity = document.querySelector("#humidity")
 var userUv = document.querySelector("#uv")
 var cityName;
+var h5El = document.getElementsByTagName("h5")
+var humidityEl = document.querySelectorAll(".cardHumidity")
+var tempEl = document.querySelectorAll(".cardTemp")
+var windEl = document.querySelectorAll(".cardWind")
 
 userCity.textContent = userCity.innerHTML + " " + today;
 
@@ -22,13 +26,16 @@ function getCoordinates(){
 
     fetch(requestUrl)
         .then(function (response) {
+            if(response===404||response===400){
+                alert("Please enter valid city")
+            }
             return response.json();
         })
         .then(function (data) {
             var lat = data.coord.lat
             var lon = data.coord.lon
             var officialCity = data.name
-            userCity.textContent=officialCity + " " + today;
+            var cityTitle = officialCity + " " + today;
             var secondRequest = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&units=imperial&APPID=cae8444643fe0b52a066739e6b318cbd'
 
             fetch(secondRequest)
@@ -36,13 +43,25 @@ function getCoordinates(){
                     return response.json();
                 })
                 .then(function (data) {
+                    userCity.textContent = cityTitle;
                     userTemp.textContent = "Temp: " + data.current.temp + "ºF";
                     userWind.textContent = "Wind: " + data.current.wind_speed + " MPH";
                     userHumidity.textContent = "Humidity: " + data.current.humidity + "%";
                     userUv.textContent = "UV Index: " + data.current.uvi;
+                    for (let i = 0; i < h5El.length; i++) {
+                        var cardDates = data.daily[i+1].dt;
+                        var differentDates = moment(cardDates, "X").format("L");
+                        h5El[i].textContent = differentDates;
+                        // daily[i] (for indexes 1-5).humidity
+                        var humidCard = data.daily[i+1].humidity;
+                        humidityEl[i].textContent = "Humidity: " + humidCard + "%";
+                        // daily.temp.max
+                        var tempCard = data.daily[i+1].temp.max
+                        tempEl[i].textContent = "Temp: " + tempCard + "ºF"
+                        // daily.wind_speed
+                        var windCard = data.daily[i+1].wind_speed;
+                        windEl[i].textContent = "Wind: " + windCard + " MPH"
+                    }
                 });
-
         });
-    
 }
-
